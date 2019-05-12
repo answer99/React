4cbc1f1172d544c59fa015e702722b840f9e7e0d
Vincent Chao <vincentchao99@gmail.com>
2019-05-12 17:11:03 +0900

    React Animations

--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Breadcrumb, BreadcrumbItem, Button, Label, Row, Col } from 'reactstrap';
 import { Link } from 'react-router-dom';
-import { Form, Errors, Control, actions } from 'react-redux-form';
+import { Form, Errors, Control} from 'react-redux-form';
 
 const required = (val) => val && val.length;
 const maxLength = (len) => (val) => !(val) || (val.length <= len);
